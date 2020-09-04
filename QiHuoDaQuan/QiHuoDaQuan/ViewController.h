cf47bbb3c1078daa9f1868f3eb57a08c2c2b0662
yangqinglong <17712121207@163.com>
2020-09-04 15:58:42 +0800

    first commit

---
+++ QiHuoDaQuan/QiHuoDaQuan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QiHuoDaQuan
+//
+//  Created by qinglong yang on 2020/9/4.
+//  Copyright © 2020 Y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QiHuoDaQuan/QiHuoDaQuanTests/QiHuoDaQuanTests.m
@@ -0,0 +1,37 @@
+//
+//  QiHuoDaQuanTests.m
+//  QiHuoDaQuanTests
+//
+//  Created by qinglong yang on 2020/9/4.
+//  Copyright © 2020 Y. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QiHuoDaQuanTests : XCTestCase
+
+@end
+
+@implementation QiHuoDaQuanTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
